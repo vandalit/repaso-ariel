5bf1957e4042b909c786feffed14ad6053f8c15d
Van <84937597+vandalit@users.noreply.github.com>
2024-12-30 15:54:03 -0300

    bootstrap

--- vue-curso-boot/src/App.vue
+++ vue-curso-boot/src/App.vue
@@ -1,6 +1,11 @@
 <template>
-
-</template>
+    <div class="container mt-5">
+      <h1 class="text-center text-primary">
+        ¡Hola desde Vue + Bootstrap!
+      </h1>
+    </div>
+  </template>
+  
 
 <style>
 
