1550e136ef4aa6d08090096255f72c0968126909
Van <84937597+vandalit@users.noreply.github.com>
2024-12-30 16:17:49 -0300

    rename multiword

--- vue-curso-boot/src/App.vue
+++ vue-curso-boot/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="bg-light">
-    <NavBar />
+    <AppNavbar />
 
     <header>
       <h1 class="text-center p-4 text-primary">Bienvenido a Cursos AlfaWeb</h1>
@@ -9,20 +9,20 @@
     <main class="container mt-4">
       <p>Contenido inicial de la aplicación</p>
     </main>
-    <Footer />
+    <AppFooter />
   </div>
 </template>
 
 
 <script>
-import NavBar from './components/AppNavbar.vue'
-import Footer from './components/AppFooter.vue'
+import AppNavbar from './components/AppNavbar.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar,
-    Footer
+    AppNavbar,
+    AppFooter
   }
 }
 </script>
--- vue-curso-boot/src/components/AppNavbar.vue
+++ vue-curso-boot/src/components/AppNavbar.vue
@@ -26,7 +26,7 @@
 
 <script>
 export default {
-  name: 'NavBar'
+  name: 'AppNavbar'
 }
 </script>
 
